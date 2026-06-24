e2e658cb4c609edf3ee38ece53cf74dc5235ca13
haseoungwon-creator <230819420+haseoungwon-creator@users.noreply.github.com>
2026-06-24 21:43:56 +0900

    (2026.6.24) 로드 밸런싱

---
+++ program/Assets/Scripts/User Interfaced/RoomStatus.cs
@@ -0,0 +1,28 @@
+using Photon.Realtime;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class RoomStatus : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Data data = new Data();
+    [SerializeField] TextMeshProUGUI roomNameText;
+    [SerializeField] TextMeshProUGUI roomIndexText;
+    [SerializeField] TextMeshProUGUI roomPersonnelText;
+
+    public void Refresh(RoomInfo roomInfo,int index)
+    {
+        data.Name = roomInfo.Name;
+        data.Index = index + 1;
+        data.PlayerCount = roomInfo.PlayerCount;
+        data.MaxPlayers = roomInfo.MaxPlayers;
+        
+        roomNameText.text = roomInfo.Name;
+        roomIndexText.text = data.Index.ToString();
+        roomPersonnelText.text = $"( { roomInfo.PlayerCount} / { data.MaxPlayers} )";
+
+
+
+    }
+    
+}
